88f2178d51335a1da7330af0ae843c8c771aabab
Tamanna <96949249+TamannaVerma99@users.noreply.github.com>
2024-07-10 12:30:35 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
-const path = require('path');
-require('dotenv').config();  // Load environment variables from .env file
-
 const app = express();
+const authRoutes = require('./routes/authRoutes');  // Adjust path as per your project structure
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Middleware
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
-// MongoDB connection
-const mongoURI = process.env.MONGO_URI;
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log('MongoDB connection error:', err));
-
-// Use routes
-app.use('/api', authRoutes);
-app.use(express.static(path.join(__dirname, '../public')));
+// Routes
+app.use('/api', authRoutes);  // Base API route
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
