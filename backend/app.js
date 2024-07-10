b24fd5025b8b84ab78c0db15836d25a5c26fe919
Anoushka Jha <95405559+BabyElias@users.noreply.github.com>
2024-07-10 14:02:48 +0530

    added files to gitignore

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,32 @@ const authRoutes = require('./routes/authRoutes');  // Adjust path as per your p
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://anoushkajha1998:Gms7oulh8nd9IyCf@myntra-cluster.qtd50qi.mongodb.net/?retryWrites=true&w=majority&appName=Myntra-cluster"
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
@@ -15,4 +41,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
