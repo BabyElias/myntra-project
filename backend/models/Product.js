52eb06dbe6e721898ac5483a6de87e5c86eaaa05
Anoushka Jha <95405559+BabyElias@users.noreply.github.com>
2024-07-11 13:02:37 +0530

    updated product model

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const productSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    default: uuidv4, // Generate a UUID for each product
+    unique: true
+  },
   name: {
     type: String,
     required: true
