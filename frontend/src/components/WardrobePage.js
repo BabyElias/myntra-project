d1755a5232d9b5af9d4f49491ea93e75afca786c
Anoushka Jha <95405559+BabyElias@users.noreply.github.com>
2024-07-14 21:03:21 +0530

    Added share wardrobe feature

--- frontend/src/components/WardrobePage.js
+++ frontend/src/components/WardrobePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { FacebookShareButton, TwitterShareButton, EmailShareButton, WhatsappShareButton } from 'react-share';
+import { FacebookIcon, TwitterIcon, EmailIcon, WhatsappIcon } from 'react-share';
 import Navbar from './Navbar';
 import './WardrobePage.css';
 import exphoto from '../chic.jpg'; // Placeholder image for wardrobe cards
@@ -16,7 +18,7 @@ const WardrobePage = () => {
     const fetchWardrobes = async () => {
       try {
         const res = await axios.get(`http://localhost:5000/api/wardrobes/${userEmail}`);
-        setWardrobes(res.data); // Assuming res.data is an array of objects {_id, name}
+        setWardrobes(res.data); // Assuming res.data is an array of objects {_id, name, thumbnail}
       } catch (error) {
         console.error('Error fetching wardrobes:', error);
       }
@@ -29,18 +31,33 @@ const WardrobePage = () => {
     <div className="wardrobe-page">
       <Navbar />
       <div className="photo-space">
-      <img src={left} alt="Left Image"/>
-        <img src={wardrobeimg} alt="Wardrobe Image" className="photo-right"c />
-        
+        <img src={left} alt="Left Image" className="photo-left" />
+        <img src={wardrobeimg} alt="Wardrobe Image" className="photo-right" />
       </div>
       <div className="wardrobes-container">
         {wardrobes.map((wardrobe) => (
-          <Link to={`/wardrobe/details/${wardrobe._id}`} key={wardrobe._id} className="wardrobe-card">
-            <img src={wardrobe.thumbnail || exphoto} alt="Wardrobe" className="wardrobe-image" />
-            <div className="wardrobe-details">
-              <h3 className="wardrobe-name">{wardrobe.name}</h3>
+          <div key={wardrobe._id} className="wardrobe-card">
+            <Link to={`/wardrobe/details/${wardrobe._id}`} className="wardrobe-link">
+              <img src={wardrobe.thumbnail || exphoto} alt="Wardrobe" className="wardrobe-image" />
+              <div className="wardrobe-details">
+                <h3 className="wardrobe-name">{wardrobe.name}</h3>
+              </div>
+            </Link>
+            <div className="share-buttons">
+              <FacebookShareButton url={`http://localhost:3000/wardrobe/details/${wardrobe._id}`}>
+                <FacebookIcon size={32} round={true} />
+              </FacebookShareButton>
+              <TwitterShareButton url={`http://localhost:3000/wardrobe/details/${wardrobe._id}`}>
+                <TwitterIcon size={32} round={true} />
+              </TwitterShareButton>
+              <EmailShareButton url={`http://localhost:3000/wardrobe/details/${wardrobe._id}`}>
+                <EmailIcon size={32} round={true} />
+              </EmailShareButton>
+              <WhatsappShareButton url={`http://localhost:3000/wardrobe/details/${wardrobe._id}`}>
+                <WhatsappIcon size={32} round={true} />
+              </WhatsappShareButton>
             </div>
-          </Link>
+          </div>
         ))}
       </div>
     </div>
